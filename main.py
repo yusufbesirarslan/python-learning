388610da6714f46d2afd61c44d7863fc5f5344c0
yusufbesirarslan <yusufbesirarslan@gmail.com>
2026-01-05 19:39:05 +0300

    Add list, dict and input examples

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ name = "Yusuf"
 age = 17
 
 print(f"My name is {name} and I am {age} years old")
+
+# List example
+numbers = [1,2,3,4,5]
+print(numbers)
+
+#Dictionary example
+
+person = {
+    "name" : "Yusuf",
+    "age" : 17,
+    "country" : "Turkey"
+}
+print(person)
+
+#Input example
+user_name = input("Enter your name: ")
+print(f"Hello, {user_name}")
